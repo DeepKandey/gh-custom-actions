ee7d4455b0f4e7f4586d397490da187982acf19f
Deepak Rai <deepak.rai2@frontdoor.com>
2023-11-12 23:10:07 +0530

    add output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,8 +11,10 @@ function run() {
 
     // Upload files
     const s3Uri = `s3://${bucket}`
-    
+
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl) //
 
     core.notice('Hello from my customa Javascript actions');
 }
